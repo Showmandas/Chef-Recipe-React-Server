9bfc2eadd2c360d5d3442023442edee7ff0a042d
Showman_Das <Showmandas3216@gmail.com>
2023-05-02 11:26:34 +0600

    json file updated

--- index.js
+++ index.js
@@ -6,9 +6,20 @@ const port = process.env.PORT || 5000
 
 const chefdata=require('./data/chefData.json')
 app.use(cors())
+app.get('/', (req, res) => {
+  res.send('Chef site is running')
+})
 app.get('/chefData', (req, res) => {
     res.send(chefdata);
   })
+
+app.get('/chefData/:id',(req,res)=>{
+  const id=parseInt(req.params.id);
+  console.log(id);
+  let selectedData=chefdata.find(data=>parseInt(data.id) === id);
+  console.log(selectedData);
+  res.send(selectedData);
+})
 app.listen(port, () => {
   console.log(`Chef recipe on port ${port}`)
 })
